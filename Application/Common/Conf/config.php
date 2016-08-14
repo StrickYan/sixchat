c649adf167de8ba3bcd422876ddfecb2bc458b20
strick <1275330626@qq.com>
2016-08-14 17:44:35 +0800

    update config.php

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -6,8 +6,8 @@ return array(
     'DB_TYPE'   => 'mysql', // 数据库类型
     'DB_HOST'   => 'localhost', // 服务器地址
     'DB_NAME'   => 'think_sixchat', // 数据库名
-    'DB_USER'   => 'S2013150028', // 用户名
-    'DB_PWD'    => '657136', // 密码
+    'DB_USER'   => 'root', // 用户名
+    'DB_PWD'    => '', // 密码
     'DB_PORT'   => 3306, // 端口
     'DB_PREFIX' => 'think_', // 数据库表前缀 
     'DB_CHARSET'=> 'utf8', // 字符集
