ab5e5eff0c7bae33ce8e43d0b170099839935905
StrickYan <1275330626@qq.com>
2017-12-11 21:02:33 +0800

    整合注册函数到auth控制器

--- Application/Home/Controller/AuthController.class.php
+++ Application/Home/Controller/AuthController.class.php
@@ -104,4 +104,27 @@ class AuthController extends Controller
         header("Location:login");
     }
 
+    /**
+     * 注册
+     */
+    public function register()
+    {
+        $id = trim($_POST['id']);
+        $password = trim($_POST['password']);
+        $idPlaceholder = "新的账号";
+        if (!empty($id) && !empty($password)) {
+            $obj = new SixChatApi2016Controller();
+            $result = $obj->register($id, $password); //调用注册api
+            if (!$result) {
+                //注册成功
+                echo "<script>window.alert('注册成功,现在登录>>');window.location.href='login';</script>";
+            } else {
+                $idPlaceholder = "该账号已存在";
+            }
+        }
+        $array['id_placeholder'] = $idPlaceholder;
+        $this->assign($array); //模板赋值
+        $this->display("Register/index"); //模板渲染
+    }
+
 }
