5efcb375b768687fa548888e088b2ccbfab309ae
yanjing05 <yanjing05@baidu.com>
2018-03-21 22:05:02 +0800

    update pwd

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -7,7 +7,7 @@ return array(
     'DB_HOST' => 'localhost', // 服务器地址
     'DB_NAME' => 'think_sixchat', // 数据库名
     'DB_USER' => 'root', // 用户名
-    'DB_PWD' => '', // 密码
+    'DB_PWD' => 'root', // 密码
     'DB_PORT' => 3306, // 端口
     'DB_PREFIX' => 'think_', // 数据库表前缀
     'DB_CHARSET' => 'utf8mb4', // 字符集
