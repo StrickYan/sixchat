93a8e2a144ba19d13982ec641dd1f31147a6e9e9
yanjing <yanjing@do-global.com>
2018-10-22 00:15:14 +0800

    update

--- Public/Home/js/moments.js
+++ Public/Home/js/moments.js
@@ -15,14 +15,17 @@
 var swiper = new Swiper('.swiper-container', {
     // 定义滚动墙参数
     pagination: '.swiper-pagination',
-    observer: true,
-    observeParents: true,
+    dynamicBullets: true,
+    observer: true, // 修改swiper自己或子元素时，自动初始化swiper
+    observeParents: true, // 修改swiper的父元素时，自动初始化swiper
     paginationClickable: true,
     spaceBetween: 0,
     centeredSlides: true,
     autoplay: 10000,
-    autoplayDisableOnInteraction: false
+    autoplayDisableOnInteraction: false,
+    loop: true,
 });
+
 var page = 1; // 上拉加载更多全局页数
 var pc_speed = 1000; // pc动画速度
 var mobile_speed = 200; // 移动端动画速度
